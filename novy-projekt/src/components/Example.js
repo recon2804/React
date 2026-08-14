f7f598f52e729e8319a3985df17724ac994776d9
David Kalny <ext.david.kalny@skoda-auto.cz>
2026-08-14 15:54:38 +0200

    multyple useStae training

--- novy-projekt/src/components/Example.js
+++ novy-projekt/src/components/Example.js
@@ -3,6 +3,7 @@ import {useState} from "react"
 
 const Example = () => {
     const [title, setTitle] = useState("Můj první titulek")
+    const [textButton, setTextButton] = useState ("Změnit text tlačítka")
 
     const buttonHandler = () => {
         setTitle("Nový titulek")
@@ -12,10 +13,20 @@ const Example = () => {
         setTitle("Znovu změněný titulek")
     }
 
+    const deleteTitle = () => {
+        setTitle("")
+    }
+
+    const changeTextButton = () => {
+        setTextButton("Text tlačítka byl změněn")
+    }
+
     return <div>
             <h2>{title}</h2>
             <button type="button" onClick={buttonHandler}>Změnit titulek</button>
             <button type="button" onClick={buttonHandlerNd}>Znovu změnit titulek</button>
+            <button type="button" onClick={deleteTitle}>Vymazat titulek</button>
+            <button type="button" onClick={changeTextButton}>{textButton}</button>
             </div>
 }
 
